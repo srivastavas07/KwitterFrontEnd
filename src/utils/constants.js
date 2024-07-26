37e022fbf132b71799007408eecaad07815173e2
Kunal Chandra <72646476+srivastavas07@users.noreply.github.com>
2024-07-27 01:57:56 +0530

    Final commit back on render

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const USER_END_POINT = "https://kwitter-backend.vercel.app/api/v1/user";
-export const TWEET_END_POINT = "https://kwitter-backend.vercel.app/api/v1/tweet";
+export const USER_END_POINT = "https://kwitterrenderbackend.onrender.com/api/v1/user";
+export const TWEET_END_POINT = "https://kwitterrenderbackend.onrender.com/api/v1/tweet";
